c2c370448ca2abdd4af8cf2bb584e5bd63013d24
J <123289295+Jomana-gh@users.noreply.github.com>
2025-08-05 12:20:34 +0300

    App.js

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import MapComponent from "./MapComponent";
 
+// Main app component rendering a header and the map component
 function App() {
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>خريطة أحياء الرياض</h2>
+      <h2 style={{ textAlign: "center" }}>Riyadh Neighborhoods Map</h2>
       <MapComponent />
     </div>
   );
 }
 
 export default App;
-
 
 
 //Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
